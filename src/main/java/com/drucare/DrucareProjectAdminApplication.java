cf61a700ac9f782ee9c49851854b92e5b86ea68e
bhuvaneswar <bhuvaneswarr412@gmail.com.com>
2024-12-12 10:05:42 +0530

    First push

---
+++ src/main/java/com/drucare/DrucareProjectAdminApplication.java
@@ -0,0 +1,13 @@
+package com.drucare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrucareProjectAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrucareProjectAdminApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/drucare/WelcomeAdmin.java
@@ -0,0 +1,14 @@
+package com.drucare;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeAdmin {
+	
+	@GetMapping
+	public String welcome() {
+		return "welcome admin version ONE";
+	}
+
+}
